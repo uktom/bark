9f74e3b9e59e310e316d7c9c8be817156f1298aa
Thomas Whitehouse <twhitehouse390@hotmail.co.uk>
2020-09-23 10:21:10 +0100

    First Bark commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var navBtn = document.querySelector('.nav__btn');
+var nav = document.querySelector('.nav ul');
+var navBtnImg = document.getElementById('navLogo');
+var closeNavBtn = document.getElementById('closeNavLogo');
+var signup = document.getElementById('signup');
+var signup2 = document.getElementById('signup2');
+var modal = document.getElementById('modal');
+var closeModal = document.getElementById('close-modal');
+
+
+navBtn.addEventListener('click', function () {
+    nav.classList.toggle('visible');
+    //console.log(navBtnImg);
+    navBtnImg.classList.toggle('hidden');
+    closeNavBtn.classList.toggle('hidden');
+});
+
+
+// Show Modal
+signup.addEventListener('click', function () {
+    modal.classList.add('show-modal');
+});
+signup2.addEventListener('click', function () {
+    modal.classList.add('show-modal');
+});
+
+//Hide modal
+closeModal.addEventListener('click', function () { modal.classList.remove('show-modal') });
+
+// Hide modal on outside click
+//window.addEventListener('click', e => e.target == modal ? modal.classList.remove('show-modal') : false);
+
+window.addEventListener('click', function (e) {
+    if (e.target == modal) {
+        modal.classList.remove('show-modal');
+    } else {
+        false
+    }
+});
